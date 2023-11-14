b43f97b2a30b1f0e2e066979d803daeddffcf0e8
MatheusMirandaM <matheusm7web@gmail.com>
2023-11-14 00:00:39 -0300

    olá

--- cypress/e2e/barriga/functional.cy.js
+++ cypress/e2e/barriga/functional.cy.js
@@ -23,7 +23,7 @@ describe('Should teste at a functional level', () => {
     
         // Por ser beforeEach, ocorre um erro caso deixar o teste abaixo dentro de um novo it
         cy.acessarMenuConta()
-        cy.xpath(loc.CONTAS.XP_BTN_ALTERAR).click()
+        cy.xpath(loc.CONTAS.FN_XP_BTN_ALTERAR('Conta teste')).click()
         cy.get(loc.CONTAS.NOME)
             .clear()
             .type('Conta alterada')
@@ -35,6 +35,23 @@ describe('Should teste at a functional level', () => {
         cy.get(loc.CONTAS.NOME).type('Conta alterada')
         cy.get(loc.CONTAS.BTN_SALVAR).click()
         cy.get(loc.MESSAGE).should('contain', '400')
+
+        // Por ser beforeEach, ocorre um erro caso deixar o teste abaixo dentro de um novo it
+        cy.get(loc.MENU.MOVIMENTACAO).click()
+        cy.get(loc.MOVIMENTACAO.DESCRICAO).type('Desc')
+        cy.get(loc.MOVIMENTACAO.VALOR).type('123')
+        cy.get(loc.MOVIMENTACAO.INTERESSADO).type('Inter')
+        cy.get(loc.MOVIMENTACAO.CONTA).select('Conta alterada')
+        cy.get(loc.MOVIMENTACAO.STATUS).click()
+        cy.get(loc.MOVIMENTACAO.BTN_SALVAR).click()
+        cy.get(loc.MESSAGE).should('contain', 'sucesso')
+
+        cy.get(loc.EXTRATO.LINHAS).should('have.length', 7)
+        cy.xpath(loc.EXTRATO.FN_XP_BUSCA_ELEMENTO('Desc', '123')).should('exist')
+
+        // Por ser beforeEach, ocorre um erro caso deixar o teste abaixo dentro de um novo it
+        cy.get(loc.MENU.HOME).click()
+        cy.xpath(loc.SALDO.FN_XP_SALDO_CONTA('Conta alterada')).should('contain', '123,00')
 
     })
 
@@ -58,4 +75,30 @@ describe('Should teste at a functional level', () => {
     //     cy.get(loc.CONTAS.BTN_SALVAR).click()
     //     cy.get(loc.MESSAGE).should('contain', '400')
     // })
+
+    // it('Should create a transaction', () => {
+        
+    //     cy.get(loc.MENU.MOVIMENTACAO).click()
+    //     cy.get(loc.MOVIMENTACAO.DESCRICAO).type('Desc')
+    //     cy.get(loc.MOVIMENTACAO.VALOR).type('123')
+    //     cy.get(loc.MOVIMENTACAO.INTERESSADO).type('Inter')
+    //     cy.get(loc.MOVIMENTACAO.STATUS).click()
+    //     cy.get(loc.MOVIMENTACAO.BTN_SALVAR).click()
+    //     cy.get(loc.MESSAGE).should('contain', 'sucesso')
+        
+    //     cy.get('.list-group').should('have.length', 7)
+    //     cy.xpath("//span[contains(.,'Desc')]/following-sibling::small[contains(., '123')]").should('exist')
+
+    //     cy.get(loc.EXTRATO.LINHAS).should('have.length', 7)
+    //     cy.xpath(loc.EXTRATO.XP_BUSCA_ELEMENTO).should('exist')
+        
+    // })
+
+    // it('Should get balance', () => {
+
+    //     cy.get(loc.MENU.HOME).click()
+    //     cy.xpath(loc.SALDO.FN_XP_SALDO_CONTA('Conta alterada')).should('contain', '123,00')
+        
+    // })
+
 })
